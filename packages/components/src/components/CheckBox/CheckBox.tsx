6c8fa0eb4c56ed3c9c6760f8ed1f5e50199c58b4
Paul Armstrong <paul@spaceyak.com>
2022-09-07 07:26:39 -0700

    fix(components): checkbox outside of group state

--- packages/components/src/components/CheckBox/CheckBox.tsx
+++ packages/components/src/components/CheckBox/CheckBox.tsx
@@ -2,7 +2,15 @@ import * as React from 'react';
 import clsx from 'clsx';
 import { useCheckboxGroupState, useToggleState } from 'react-stately';
 import type { CheckboxGroupState } from 'react-stately';
-import { useFocusRing, useCheckbox, useCheckboxGroup, useCheckboxGroupItem, VisuallyHidden } from 'react-aria';
+import {
+	mergeProps,
+	useFocusRing,
+	useCheckbox,
+	useCheckboxGroup,
+	useCheckboxGroupItem,
+	useId,
+	VisuallyHidden,
+} from 'react-aria';
 import type { AriaCheckboxGroupProps, AriaCheckboxGroupItemProps, AriaCheckboxProps } from 'react-aria';
 import { useIcons } from '../../context';
 
@@ -29,39 +37,76 @@ export function CheckBoxGroup({ children, ...props }: GroupProps) {
 
 interface Props {
 	value: string;
+	errorMessage?: React.ReactNode;
+	description?: React.ReactNode;
 }
 type CheckboxProps = Props & AriaCheckboxProps;
 type CheckboxGroupProps = Props & AriaCheckboxGroupItemProps;
 
 export function CheckBox(props: CheckboxProps | CheckboxGroupProps) {
 	const groupState = React.useContext(CheckBoxContext);
+	const state = useToggleState(props);
 	const { CheckIcon } = useIcons();
 	const ref = React.useRef<HTMLInputElement>(null);
 	const { inputProps } = groupState
 		? useCheckboxGroupItem(props as AriaCheckboxGroupItemProps, groupState, ref) // eslint-disable-line react-hooks/rules-of-hooks
-		: useCheckbox(props, useToggleState(props), ref); // eslint-disable-line react-hooks/rules-of-hooks
+		: useCheckbox(props, state, ref); // eslint-disable-line react-hooks/rules-of-hooks
 	const { isFocusVisible, focusProps } = useFocusRing();
+	const errorId = useId();
+	const descriptionId = useId();
 
-	const isDisabled = groupState.isDisabled || props.isDisabled;
-	const isSelected = groupState.isSelected(props.value);
+	const isDisabled = groupState?.isDisabled || props.isDisabled;
+	const isSelected = groupState ? groupState.isSelected(props.value) : state.isSelected;
+	const { errorMessage, description } = props;
 
 	return (
-		<label className="flex w-full cursor-pointer items-center justify-center gap-2">
-			<VisuallyHidden>
-				<input {...inputProps} {...focusProps} className="absolute left-48" ref={ref} />
-			</VisuallyHidden>
-			<div
-				className={clsx(
-					'flex h-4 w-4 rounded-sm border-2 text-white transition-colors duration-150',
-					isFocusVisible && 'ring-4 ring-primary-500 ring-opacity-50',
-					isDisabled && 'border-gray-200 bg-gray-100',
-					isSelected ? 'border-primary-500 bg-primary-500 text-white' : !isDisabled && 'border-gray-300 bg-white'
-				)}
-			>
-				{isSelected ? <CheckIcon /> : null}
-			</div>
+		<div className="w-full">
+			<label className="flex w-full cursor-pointer items-center justify-center gap-2">
+				<VisuallyHidden>
+					<input
+						{...mergeProps(inputProps, focusProps)}
+						aria-invalid={Boolean(errorMessage)}
+						aria-describedby={
+							errorMessage || description
+								? `${errorMessage ? errorId : ''} ${description ? descriptionId : ''}`
+								: undefined
+						}
+						className="absolute left-48"
+						ref={ref}
+					/>
+				</VisuallyHidden>
 
-			<div className={clsx('inline-flex flex-grow leading-3', isDisabled && 'text-gray-600')}>{props.children}</div>
-		</label>
+				<div
+					className={clsx(
+						'flex h-4 w-4 rounded-sm border-2 text-white transition-colors duration-150',
+						isFocusVisible && 'ring-4 ring-primary-500 ring-opacity-50',
+						isDisabled && 'border-gray-200 bg-gray-100',
+						isSelected ? 'border-primary-500 bg-primary-500 text-white' : !isDisabled && 'border-gray-300 bg-white'
+					)}
+				>
+					{isSelected ? <CheckIcon /> : null}
+				</div>
+
+				<div className={clsx('inline-flex flex-grow leading-3', isDisabled && 'text-gray-600')}>
+					{props.children}
+					{props.isRequired ? (
+						<span aria-hidden="true" className="text-red-600">
+							*
+						</span>
+					) : null}
+				</div>
+			</label>
+
+			{description ? (
+				<div id={descriptionId} className="pl-6 pt-1 text-xs">
+					{description}
+				</div>
+			) : null}
+			{errorMessage ? (
+				<div id={errorId} className="pl-6 pt-1 text-sm text-red-600">
+					{errorMessage}
+				</div>
+			) : null}
+		</div>
 	);
 }
